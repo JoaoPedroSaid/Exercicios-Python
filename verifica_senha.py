027337da63659c739461dc56bfbab747b1db76ae
JoaoPedroSaid <jottapp090@gmail.com>
2025-07-12 02:17:08 -0300

    Add files via upload

---
+++ verifica_senha.py
@@ -0,0 +1,28 @@
+def verificarSenha(txt):
+    if len(txt) < 6:
+        print('A senha precisa ter pelo menos 6 digitos.')
+
+    temLetra = False
+    temNumero = False
+
+    for c in txt:
+        if c.isalpha():
+            temLetra = True
+        if c.isdigit():
+            temNumero = True
+
+    if not temLetra:
+        print('Senha precisa ter pelo menos 1 letra.')
+        return False
+    if not temNumero:
+        print('Senha precisa possuir ter pelo menos 1 numero.')
+        return False
+    else: 
+        return True
+
+while True:
+    senha = str(input('Escreva uma senha: '))
+
+    if verificarSenha(senha):
+        print('Senha valida.')
+        break
