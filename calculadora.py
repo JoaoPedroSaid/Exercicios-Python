406a85457be5be1cbaafffd7b4761c7a4c16a5cd
JoaoPedroSaid <jottapp090@gmail.com>
2025-07-11 22:47:35 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,56 @@
+print('----------------------\nCALCULADORA EM PYTHON\n----------------------')
+
+def som(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
+
+def div(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erro: divisão por zero"
+
+while True:
+    print('-------------------------------\nCALCULADORA EM PYTHON\n-------------------------------\n[1] Soma')
+    print('[2] Subtração')
+    print('[3] Multiplicação')
+    print('[4] Divisão')
+    print('[0] Sair')
+    print('-------------------------------')
+
+    operação = int(input('Qual equação você deseja fazer? '))
+
+    if operação == 0:
+        print('Encerrando a calculadora.')
+        break  # Sai do laço
+
+    if operação not in [1, 2, 3, 4]:
+        print('Opção inválida. Tente novamente.')
+        continue
+
+    num1 = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
+
+    if operação == 1:
+        total = som(num1, num2)
+        equação = f'{num1} + {num2}'
+    elif operação == 2:
+        total = sub(num1, num2)
+        equação = f'{num1} - {num2}'
+    elif operação == 3:
+        total = multi(num1, num2)
+        equação = f'{num1} * {num2}'
+    elif operação == 4:
+        total = div(num1, num2)
+        equação = f'{num1} / {num2}'
+
+    print('-------------------------------')
+    print('Resultado')
+    print('-------------------------------')
+    print(f'Equação: {equação}\nResultado: {total}')
+    print('-------------------------------\n')
---
+++ poupança.py
@@ -0,0 +1,8 @@
+valorInicial = float(input('Quantos R$ você deseja depositar na sua poupança? '))
+valorAtual = valorInicial
+import locale
+locale.setlocale(locale.LC_ALL,'pt_BR.UTF-8') # isso é para conseguir formatar datas, valores, etc. no padrão brasileiro
+
+for mes in range(1, 13):
+    valorAtual = valorAtual * (1 + 0.005) 
+    print(f'No {mes}º você possui {locale.currency(valorAtual, grouping=True)}') 
